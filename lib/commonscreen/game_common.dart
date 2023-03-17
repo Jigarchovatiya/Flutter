26298231c457b30a571915a406272a6c2e93cdb2
jigar chovatiya <115682188+Jigarchovatiya@users.noreply.github.com>
2023-03-17 14:34:31 +0530

    17-03-23

--- lib/commonscreen/game_common.dart
+++ lib/commonscreen/game_common.dart
@@ -5,47 +5,45 @@ class GameCommon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.only(top: 20),
-        child: ListView.separated(
-          separatorBuilder: (context, index) => const SizedBox(
-            height: 20,
-          ),
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: 3,
-          itemBuilder: (context, index) => Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              children: [
-                Image.asset(
-                  "assets/eight/remote_image.png",
-                  width: 88,
-                ),
-                const SizedBox(width: 20),
-                RichText(
-                  text: const TextSpan(
-                    text: "\$49.99\n",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w700,
-                      color: Color(0xFF36322A),
-                    ),
-                    children: [
-                      TextSpan(
-                        text: "Product Name",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400,
-                          color: Color(0xFF36322A),
-                        ),
-                      ),
-                    ],
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: ListView.separated(
+        separatorBuilder: (context, index) => const SizedBox(
+          height: 20,
+        ),
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: 3,
+        itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            children: [
+              Image.asset(
+                "assets/eight/remote_image.png",
+                width: 88,
+              ),
+              const SizedBox(width: 20),
+              RichText(
+                text: const TextSpan(
+                  text: "\$49.99\n",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xFF36322A),
                   ),
+                  children: [
+                    TextSpan(
+                      text: "Product Name",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xFF36322A),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
