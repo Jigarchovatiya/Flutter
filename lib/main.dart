da999e61e6d2693128454fee24c02faafddfcd98
jigar chovatiya <115682188+Jigarchovatiya@users.noreply.github.com>
2023-03-21 15:28:44 +0530

    21-03-23

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:screens_app/shoes_screen.dart';
+import 'package:screens_app/shop_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: const ShoesScreen(),
+      home: const ShopScreen(),
     );
   }
 }
